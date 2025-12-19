b2d081d144b798ce19d2f72c5f3b8932832666d2
Devnity <saifurrehman7224@gmail.com>
2025-12-19 22:25:33 +0500

    updates

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -8,15 +8,6 @@ import { getFirestore, collection, doc, setDoc, getDocs, getDoc, query, where, o
 from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
 
 // ✅ Your Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyC7x2FU1YpxXTSPlnitvIwjw9DOLcko_ag",
-//   authDomain: "glowmart-3c283.firebaseapp.com",
-//   projectId: "glowmart-3c283",
-//   storageBucket: "glowmart-3c283.appspot.com",
-//   messagingSenderId: "703685595663",
-//   appId: "1:703685595663:web:4cca60825479d9326b097a",
-//   measurementId: "G-T3TD3942VK"
-// };
 const firebaseConfig = {
   apiKey: "AIzaSyAvYkFQ0KWvSpz9X4o-ht_-13LLiZRe0jM",
   authDomain: "glowmart-16b36.firebaseapp.com",
@@ -26,6 +17,7 @@ const firebaseConfig = {
   appId: "1:863861686880:web:210523eef6b1ba31fff905",
   measurementId: "G-7B2T1Z17SJ"
 };
+
 // ✅ Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
